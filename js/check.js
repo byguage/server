2c6a613afaf9bf0051c31cb147fc589f60daa8fe
byguage <110472388+byguage@users.noreply.github.com>
2024-03-18 23:10:15 +0800

    Update check.js
    Signed-off-by: byguage <110472388+byguage@users.noreply.github.com>

--- js/check.js
+++ js/check.js
@@ -42,63 +42,62 @@ fullScreenButton.addEventListener('click', function() {
     }
 });
 function executeMinecraftLink(event) {
-    if (!shouldExecuteMinecraftLink(event)) {
-        event.preventDefault();
-    } else {
-        openMinecraft(event);
+        if (!shouldExecuteMinecraftLink(event)) {
+            event.preventDefault();
+        } else {
+            openMinecraft(event);
+        }
     }
-}
-document.getElementById('minecraftLink').addEventListener('click', function(event) {
-    var ua = navigator.userAgent.toLowerCase(); 
-    var conf = { qqjump: 1 };
-    // if (ua.match(/QQ/i) == "qq") {
-    if ((ua.indexOf('QQ/') !== -1 || ua.match(/QQ/i) == "qq" || ua.indexOf('MicroMessenger') !== -1) && conf.qqjump === 1) {
+    function shouldExecuteMinecraftLink(event) {
+        var ua = navigator.userAgent.toLowerCase(); 
+        var conf = { qqjump: 1 };
+        if ((ua.indexOf('QQ/') !== -1 || ua.match(/QQ/i) == "qq" || ua.indexOf('MicroMessenger') !== -1) && conf.qqjump === 1) {
         alert('点击右上角跳转其他浏览器打开');
         var scriptElement = document.createElement('script');
         scriptElement.src = 'https://open.mobile.qq.com/sdk/qqapi.js?_bid=152';
         document.head.appendChild(scriptElement);
         mqq.ui.openUrl({ target: 2, url:"https://www.mc.sccc.top"});
         return false; 
-    }
-    return true; 
-}
-function getDeviceType() {
-    var userAgent = navigator.userAgent.toLowerCase();
-    if (/windows/.test(userAgent)) {
-        return 'Windows';
-    } else if (/iphone|ipad|ipod/.test(userAgent)) {
-        return 'iOS';
-    } else if (/android/.test(userAgent)) {
-        return 'Android';
-    } else {
-        return 'Unknown';
-    }
-}
-function openMinecraft(event) {
-    event.preventDefault(); 
-    var isMinecraftInstalled = false;
-    var iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.src = 'minecraft://';
-    document.body.appendChild(iframe);
-    setTimeout(function() {
-        if (document.body.contains(iframe)) {
-            alert('您的系统中未安装 Minecraft 或者无法处理 minecraft:// 协议链接。');
-            var deviceType = getDeviceType();
-            switch(deviceType) {
-                case 'Windows':
-                    window.location.href = 'https://mc.minebbs.com/#/windows';
-                    break;
-                case 'iOS':
-                    window.location.href = 'https://mc.minebbs.com/#/ios';
-                    break;
-                case 'Android':
-                    window.location.href = 'https://mc.minebbs.com/#/';
-                    break;
-                default:
-                    break;
-            }
         }
-        document.body.removeChild(iframe); 
-    }, 100); 
-}
+        return true; 
+    }
+    function getDeviceType() {
+        var userAgent = navigator.userAgent.toLowerCase();
+        if (/windows/.test(userAgent)) {
+            return 'Windows';
+        } else if (/iphone|ipad|ipod/.test(userAgent)) {
+            return 'iOS';
+        } else if (/android/.test(userAgent)) {
+            return 'Android';
+        } else {
+            return 'Unknown';
+        }
+    }
+    function openMinecraft(event) {
+        event.preventDefault(); 
+        var isMinecraftInstalled = false;
+        var iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        iframe.src = 'minecraft://';
+        document.body.appendChild(iframe);
+        setTimeout(function() {
+            if (document.body.contains(iframe)) {
+                alert('您的系统中未安装 Minecraft 或者无法处理 minecraft:// 协议链接。');
+                var deviceType = getDeviceType();
+                switch(deviceType) {
+                    case 'Windows':
+                        window.location.href = 'https://mc.minebbs.com/#/windows';
+                        break;
+                    case 'iOS':
+                        window.location.href = 'https://mc.minebbs.com/#/ios';
+                        break;
+                    case 'Android':
+                        window.location.href = 'https://mc.minebbs.com/#/';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            document.body.removeChild(iframe); 
+        }, 100); 
+    }
